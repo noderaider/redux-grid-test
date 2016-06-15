66dbaf3b7f2b26171bf4378d2215196a284f2c5d
Cole Chamberlain <cole.chamberlain@gmail.com>
2016-06-14 17:02:48 -0700

    passing 6 unit tests

--- test/Grid.js
+++ test/Grid.js
@@ -1,7 +1,8 @@
 import reduxGrid from 'redux-grid'
 import React from 'react'
 import * as ReactVirtualized from 'react-virtualized'
-import { connect } from 'react-redux'
+import { createStore } from 'redux'
+import { Provider, connect } from 'react-redux'
 import { mount, shallow } from 'enzyme'
 import { spy } from 'sinon'
 const chai = require('chai')
@@ -14,30 +15,11 @@ describe('reduxGrid', () => {
   it('throws for no parameters', () => (() => reduxGrid()).should.throw())
   it('should not throw for valid dependencies (React, ReactVirtualized, connect)', () => (() => reduxGrid({ React, ReactVirtualized, connect })).should.not.throw())
   describe('<Grid />', () => {
+    const reducer = (state = {}, action = {}) => {}
+    const store = createStore(reducer)
     const { Grid } = reduxGrid({ React, ReactVirtualized, connect })
     it('should exist', () => should.exist(Grid))
-  })
-  xdescribe('componentDidMount', () => {
-    it('mounts', () => {
-      spy(ApiGrid.prototype, 'componentDidMount')
-      const wrapper = mount(<ApiGrid />)
-      ApiGrid.prototype.componentDidMount.calledOnce.should.be.true
-    })
-  })
-  xdescribe('render', () => {
-    context('renders direct data', () => {
-      const data =  [ { name: 'jim', age: 26, interest: 'being boring', sex: 'male' }
-                    , { name: 'tony', age: 37, interest: 'skydiving', sex: 'male' }
-                    , { name: 'lisa', age: 40, interest: 'sleeping', sex: 'female' }
-                    , { name: 'dan', age: 20, interest: 'jumping', sex: 'male' }
-                    , { name: 'sarah', age: 15, interest: 'eating', sex: 'female' }
-                    ]
-
-      const selector = state => state.data.map(({ name, age, interest, sex }) => ({ ['Name']: name, ['Age']: age, ['Is A Dude']: sex === 'male' }))
-      it('renders same number of columns as selector', () => {
-        const wrapper = shallow(<ApiGrid selector={selector} state={data} />)
-      })
-    })
+    it('should shallow mount', () => (() => shallow(<Provider store={store}><Grid /></Provider>)).should.not.throw())
   })
 })
 
